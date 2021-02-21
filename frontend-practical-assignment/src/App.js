819c6c3a49d275446d1d9ac71af64b65aea524a5
Michal <karnath.michal@gmail.com>
2021-02-21 20:09:15 +0100

    routing,component List and Fav added to project

--- frontend-practical-assignment/src/App.js
+++ frontend-practical-assignment/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Fav from "./Fav";
+import List from "./List";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/fav">
+              <Fav />
+            </Route>
+            <Route path="/">
+              <List />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
