6e8d399288101e48e03408be330dfa989d1d7e9b
Michal <karnath.michal@gmail.com>
2021-02-21 20:18:57 +0100

    data fetched from API and dispalyed on List component

--- frontend-practical-assignment/src/List.js
+++ frontend-practical-assignment/src/List.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 const List = () => {
+  const [rates, setRates] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://api.nbp.pl/api/exchangerates/tables/a/")
+      .then((response) => response.json())
+      .then((data) => {
+        setRates(data[0]);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <h1>List</h1>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Code</TableCell>
+                <TableCell>Rate mid</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rates.rates.map((rate, index) => {
+                return (
+                  <TableRow key={index}>
+                    <TableCell>{rate.currency}</TableCell>
+                    <TableCell>{rate.code}</TableCell>
+                    <TableCell>{rate.mid}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </>
+      )}
     </div>
   );
 };
